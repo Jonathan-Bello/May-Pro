d0ac8b0f1dd5c0adac696f828062327c4be646a6
Jonathan Alexis Bello Lopez <jonathan.bellofg@gmail.com>
2024-07-22 11:02:15 -0600

    add: create header and nav menu

---
+++ tailwind.config.mjs
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: '"Raleway", sans-serif',
+        title: '"Poppins", serif',
+      },
+      colors: {
+        primary: {
+          1: "#D82227",
+          2: "#B11E29",
+          3: "#AD2224",
+          4: "#701F23",
+        },
+        brand: {
+          red: "#A82327",
+          grey: "#433F40",
+        },
+        system: {
+          white50: "#ffffff80",
+          bg: "#FDFDFD",
+          bgHeader: "#F9F9F9",
+          grey: "#CACACA",
+        },
+        grey: {
+          1: "#1E130F",
+          2: "#3C3C3E",
+          3: "#403E41",
+          4: "#999A9B",
+          5: "#E3E3E3",
+          6: "#ffffff",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
