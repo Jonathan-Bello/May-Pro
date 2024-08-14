90fa1e25384c41172f23184e57381abe33b4be7a
Jonathan Alexis Bello Lopez <jonathan.bellofg@gmail.com>
2024-08-14 00:59:14 -0600

    add:item list and second section

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,8 +4,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: '"Raleway", sans-serif',
-        title: '"Poppins", serif',
+        sans: '"Poppins", sans-serif',
+        title: '"Raleway", serif',
       },
       colors: {
         primary: {
