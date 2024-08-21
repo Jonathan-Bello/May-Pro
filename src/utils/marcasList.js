38604c2e04589c094fe0eb1c9f95e1ff008e8c97
Jonathan Alexis Bello Lopez <jonathan.bellofg@gmail.com>
2024-08-20 23:14:38 -0600

    add: finish home page

---
+++ src/utils/marcasList.js
@@ -0,0 +1,51 @@
+import imgApm from "@/assets/img/home/apm-terminals.webp";
+import imgArpada from "@/assets/img/home/arpada.webp";
+import imgAttico7 from "@/assets/img/home/attico7.webp";
+import imgBeck from "@/assets/img/home/beck.webp";
+import imgCarso from "@/assets/img/home/carso.webp";
+import imgCotecon from "@/assets/img/home/contecon.webp";
+import imgDlabra from "@/assets/img/home/dlabra.webp";
+import imgEcsa from "@/assets/img/home/ecsa.webp";
+import imgFg2 from "@/assets/img/home/fg2.webp";
+import imgGrupoImpulsa from "@/assets/img/home/grupo-impulsa.webp";
+import imgGsm from "@/assets/img/home/gsm.webp";
+import imgHazesa from "@/assets/img/home/hazesa.webp";
+import imgHogarUnion from "@/assets/img/home/hogarunion.webp";
+import imgHomeDepot from "@/assets/img/home/homedepot.webp";
+import imgInterarq from "@/assets/img/home/interarq.webp";
+import imgJaver from "@/assets/img/home/javer.webp";
+import imgLebanc from "@/assets/img/home/lebanc.webp";
+import imgMigdal from "@/assets/img/home/migdal.webp";
+import imgMt from "@/assets/img/home/mt.webp";
+import imgParks from "@/assets/img/home/parks.webp";
+import imgPulso from "@/assets/img/home/pulso.webp";
+import imgQuieroCasa from "@/assets/img/home/quierocasa.webp";
+import imgSmc from "@/assets/img/home/smc.webp";
+import imgSsa from "@/assets/img/home/ssa.webp";
+
+export default [
+  imgSsa,
+  imgCotecon,
+  imgApm,
+  imgHazesa,
+  imgLebanc,
+  imgQuieroCasa,
+  imgGsm,
+  imgMt,
+  imgFg2,
+  imgGrupoImpulsa,
+  imgSmc,
+  imgJaver,
+  imgPulso,
+  imgCarso,
+  imgInterarq,
+  imgEcsa,
+  imgArpada,
+  imgDlabra,
+  imgHogarUnion,
+  imgAttico7,
+  imgMigdal,
+  imgHomeDepot,
+  imgBeck,
+  imgParks,
+];
