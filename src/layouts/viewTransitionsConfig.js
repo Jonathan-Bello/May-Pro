95ed3fb558ecd99e9dfa771a7f164650654a009b
Jonathan Alexis Bello Lopez <jonathan.bellofg@gmail.com>
2024-08-28 01:15:00 -0600

    add: scroll condicion del home

--- src/layouts/viewTransitionsConfig.js
+++ src/layouts/viewTransitionsConfig.js
@@ -1,15 +1,20 @@
 let scrollPosition = null;
 
 document.addEventListener("astro:before-swap", (ev) => {
+  // *Lista de paginas que mantendrán el scroll
   const exceptions = [
     "/productos/morteros-secos-premezclados",
     "/productos/estucos",
     "/productos/adhesivos",
   ];
 
-  exceptions.includes(ev.to.pathname) && (scrollPosition = window.scrollY);
+  // * Indicamos que si el path no es el home, entonces se mantiene el scroll de la pagina anterior
+  ev.from.pathname !== "/" &&
+    exceptions.includes(ev.to.pathname) &&
+    (scrollPosition = window.scrollY);
 });
 
+//  Limpia el scroll cuando se navega a otra pagina
 document.addEventListener("astro:after-swap", () => {
   scrollPosition && window.scrollTo(0, scrollPosition);
   scrollPosition = null;
